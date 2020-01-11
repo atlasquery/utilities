5a3f417fb99c1711a8b16e7550fa59f132f3042a
thegove <gove.allen@gmail.com>
2020-01-11 09:48:56 -0700

    one ad

--- ads.js
+++ ads.js
@@ -1,8 +1,6 @@
 ads=[
-    {html:{__html:'<div style="text-align:center; padding:.5rem;">Instructions for this Add-in</div>'},
-    url:"https://support.atlasquery.com"
-   },
-   {html:{__html:'ad two'},
-   url:"https://google.com"
-  },
+    {
+       html:{__html:'<div style="text-align:center; padding:.5rem;">Instructions for this Add-in</div>'},
+       url:"https://support.atlasquery.com"
+   }
 ]
