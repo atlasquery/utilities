1ff6cf85a5d223aa4b6cec599ed0f24682ef2e2a
thegove <gove.allen@gmail.com>
2020-01-11 09:25:06 -0700

    two ads

--- ads.js
+++ ads.js
@@ -1,5 +1,8 @@
 ads=[
-        {html:{__html:'Thanks for trying my  add-in.  Instructions can be found at: https://support.atlasquery.com'},
-         url:"https://byu.edu"
-        },
-      ]
+    {html:{__html:'Thanks for trying my  add-in.  Instructions can be found at: https://support.atlasquery.com'},
+    url:"https://byu.edu"
+   },
+   {html:{__html:'ad two'},
+   url:"https://google.com"
+  },
+]
