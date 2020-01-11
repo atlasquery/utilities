0f34d5f95e514df6801d1c54b9178ea8223a9672
thegove <gove.allen@gmail.com>
2020-01-11 09:36:40 -0700

    removed extraneos space

--- ads.js
+++ ads.js
@@ -1,5 +1,5 @@
 ads=[
-    {html:{__html:'Thanks for trying my  add-in.  Instructions can be found at: https://support.atlasquery.com'},
+    {html:{__html:'Thanks for trying my add-in.  Instructions can be found at: https://support.atlasquery.com'},
     url:"https://byu.edu"
    },
    {html:{__html:'ad two'},
